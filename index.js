fd8e68e250ac617cc097f5359926c96c6070e7e1
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-16 00:34:08 +0300

    changes

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const quoteEl = document.getElementById("quote");
 const authorEl = document.getElementById("author");
 const apiURL = "https://api.quotable.io/random";
 
+
  async function getQuote(){
-    const response = await fetch(apiURL);
-    const data = await response.json();
-    const quoteContent = data.content;
-    const quoteAuthor = data.author;
-    quoteEl.innerText = quoteContent;
-    authorEl.innerText = "~ " + quoteAuthor;
-    console.log(data);
+
+    try{
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        const quoteContent = data.content;
+        const quoteAuthor = data.author;
+        quoteEl.innerText = quoteContent;
+        authorEl.innerText = "~ " + quoteAuthor;
+        console.log(data);
+    }catch (error){
+        
+    }
+    
 
 }
 
